2530d6a9e6a4d17bd0d8fc02b604d0aca2f8cd42
Benjamin Morse <bjmorse@gmail.com>
2021-05-15 23:33:54 -0700

    comment only

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?
 include("include/include.php");
 //loadPage( "content", "right", "page title");
+
+// these two lines give me an unset var, then I can use
+// isset() inside
 $mynull= "";
 unset($mynull);
+
 loadPage("index.tpl", "index_right.tpl", "Home", $mynull);
 
 ?>
